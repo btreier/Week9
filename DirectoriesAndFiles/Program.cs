237a78bff2842b0205e86c191a41b1fe47b491f3
Birgith Maria Treier <btreier@tktk.ee>
2021-12-05 11:34:22 +0200

    add new projects

---
+++ DirectoriesAndFiles/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace DirectoriesAndFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootDirectory = @"C:\Users\...\Desktop\Programmeerimine\samples";
+            Console.WriteLine("Enter directory name:");
+            string newDirectory = Console.ReadLine();
+            Console.WriteLine("Enter file name:");
+            string fileName = Console.ReadLine();
+
+
+            if (Directory.Exists($"{rootDirectory}\\{newDirectory}") && File.Exists($"{rootDirectory}\\{newDirectory}\\{fileName}"))
+            {
+                Console.WriteLine($"Directory {newDirectory} and File {fileName} already exists at {rootDirectory}");
+            }
+            else
+            {
+                Directory.CreateDirectory($"{rootDirectory}\\{newDirectory}");
+                File.Create($"{rootDirectory}\\{newDirectory}\\{fileName}");
+            }
+        }
+    }
+}
